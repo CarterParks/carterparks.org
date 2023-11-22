8fc1cbf52b963078c4f74e8303be4e0d1e1a480d
Carter Parks <mail@carterparks.org>
2023-11-22 15:30:36 -0700

    Source update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
-let cols = []
-document.querySelectorAll(".card").forEach((x, i) => {
-  let hue = i * 20 % 360;
-  x.addEventListener('mouseover', (e) => {
-    document.documentElement.style.setProperty('--highlight', `oklch(.75 .12 ${hue})`);
-  });
-  x.addEventListener('mouseout', (e) => {
-    e.target.style.color = null;
-  });
-});
+fetch("content.txt")
+  .then((res) => res.text())
+  .then((text) => {
+    let content =  text.split(/\n\*/).slice(1);
+    while (content.length > 0) {
+      let cardHead = document.createElement('div');
+      cardHead.classList.add("card-header");
+      cardHead.innerHTML = content.shift();
 
+      let cardBody = document.createElement('div');
+      cardBody.classList.add("card-body");
+      cardBody.innerHTML = content.shift();
+
+      let card = document.createElement('div');
+      card.classList.add("card");
+      card.prepend(cardBody);
+      card.prepend(cardHead);
+
+      document.querySelector('#container').appendChild(card);
+    }
+
+    let cards = document.querySelectorAll('.card');
+    cards.forEach((x, i) => x.style.setProperty('--hue', `${(i / cards.length) * 360 }`));
+  })
+  .catch((e) => console.error(e));
--- sync.sh
+++ sync.sh
@@ -1,2 +1,7 @@
 #!/bin/zsh
-aws s3 sync --acl 'public-read' --exclude '.git/*' --exclude 'sync.sh' --delete . s3://carterparks.org/
+aws s3 sync\
+  --acl 'public-read'\
+  --exclude '.git/*'\
+  --exclude 'sync.sh'\
+  --delete\
+  . s3://carterparks.org/
